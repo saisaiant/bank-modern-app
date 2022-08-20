d8f99a58da9d1dcdd5dd40075dd3efa972dda282
kosai <saisaiant@gmail.com>
2022-08-20 12:11:45 +0630

    Styling Hero Compoents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import styles from './style'
-
-import { Navbar, Billing, CardDeal, Business, Clients, CTA,
-  Stats, Footer, Testimonials, Hero } from './components'
-
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
@@ -18,22 +14,20 @@ const App = () => (
         <Hero />
       </div>
     </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
-        <CardDeal /> 
-        <Testimonials /> 
-        <Clients /> 
-        <CTA /> 
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
         <Footer />
       </div>
     </div>
-
   </div>
-)
+);
 
-
-export default App
+export default App;
